b78c9e663393c237e88ee0724c2fd502f5f2e1b9
Dylan van Bunnik <bungohdev@gmail.com>
2023-12-13 11:32:20 -0500

    add status endpoint.

--- src/main/java/dev/dpvb/MeepBot.java
+++ src/main/java/dev/dpvb/MeepBot.java
@@ -10,29 +10,44 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import spark.Spark;
 
 public class MeepBot {
+
+    private static JDA jda;
 
     public static void main(String[] args) throws InterruptedException {
         final Dotenv dotenv = Dotenv.load();
         final String TOKEN = dotenv.get("DISCORD_TOKEN");
-        final JDA jda = JDABuilder
+        jda = JDABuilder
                 .createDefault(TOKEN)
-                .setActivity(Activity.customStatus("i love flare"))
+                .setActivity(Activity.customStatus("mega pace"))
                 .enableIntents(GatewayIntent.GUILD_MEMBERS)
                 .build();
 
-        jda.updateCommands().addCommands(
-                Commands.slash("hey", "Say hey!")
-        ).queue();
+        registerEvents();
+        registerCommands();
+        setupServer();
 
+        jda.awaitReady();
+    }
+
+    private static void registerEvents() {
         jda.addEventListener(new ReadyListener());
         jda.addEventListener(new MessageListener());
         jda.addEventListener(new JoinListener());
+    }
 
+    private static void registerCommands() {
+        jda.updateCommands().addCommands(
+                Commands.slash("hey", "Say hey!")
+        ).queue();
         jda.addEventListener(new HeyCommand());
+    }
 
-        jda.awaitReady();
+    private static void setupServer() {
+        Spark.port(4567);
+        Spark.get("/status", (req, res) -> "Online");
     }
 
 }
