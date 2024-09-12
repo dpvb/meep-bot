d8d44895a4d717679ee7713e9f77178464152d47
dbunnik3 <dbunnik3@gatech.edu>
2024-09-11 21:48:39 -0400

    change commands

--- src/main/java/dev/dpvb/commands/InsultsCommand.java
+++ src/main/java/dev/dpvb/commands/InsultsCommand.java
@@ -4,17 +4,27 @@ import dev.dpvb.mongo.MongoManager;
 import dev.dpvb.mongo.services.InsultSuggestionService;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 
-public class InsultsCommand extends ListenerAdapter {
+public class InsultsCommand extends Command {
 
     private final InsultSuggestionService iss;
 
     public InsultsCommand() {
+        super("insults", "List all of Brownie's insults!");
         iss = MongoManager.getInstance().getInsultSuggestionService();
     }
 
+    @Override
+    public SlashCommandData generateSlashCommand() {
+        return Commands.slash(getName(), getDescription())
+                .setDefaultPermissions(DefaultMemberPermissions.DISABLED);
+    }
+
     public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
-        if (event.getName().equals("insults")) {
+        if (event.getName().equals(getName())) {
             // loop through approved messages and store them in a string
             StringBuilder sb = new StringBuilder();
             sb.append("# Approved Insults\n");
